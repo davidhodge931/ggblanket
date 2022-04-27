520b282e8e07edf6e0cfd0b673871d65776c106f
davidhodge931 <davidhodge931@gmail.com>
2022-04-27 18:41:53 +1200

    Add logo

--- R/logo.R
+++ R/logo.R
@@ -1,30 +1,42 @@
 library(tidyverse)
 library(hexSticker)
-library(ggblanket)
-library(palmerpenguins)
+library(simplevis)
 
-plot <- iris |>
-  gg_density(x = Sepal.Length,
-             col = Species,
-             alpha = 1,
-             position = "stack",
-             col_legend_place = "n")
+nz <- simplevis::example_borders %>%
+  mutate(dummy = "1") %>%
+  sf::st_as_sf()
+
+plot <- gg_sf_col(
+  nz,
+  dummy,
+  title = "",
+  pal = "#f8fafb",
+  col_labels = "",
+  col_title = ""
+) +
+  theme(panel.background = element_rect(fill = "#EAEFF3")) +
+  theme(panel.grid = element_blank())
+
+plot
 
 sticker(
   plot,
   package = "ggblanket",
-  p_color = "white",
-  h_color = ggblanket:::pal_viridis_reorder(1),
+  p_color = simplevis:::pal_viridis_reorder(1),
+  h_color = simplevis:::pal_viridis_reorder(1),
   h_fill = "#C1D1DD",
   p_size = 25,
+  p_family = "Helvetica",
   p_y = 1.1,
   p_x = 1.0,
-  s_width = 7,
-  s_height = 30,
+  s_x = 0.3,
+  s_y = 0.74,
+  s_width = 10,
+  s_height = 15,
   filename = "logo.png",
   white_around_sticker = TRUE
 )
 
 usethis::use_logo("logo.png")
 
-rm("logo.png")
+file.remove("logo.png")
