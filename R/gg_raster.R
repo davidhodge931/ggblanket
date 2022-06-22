049bf28032f978276e6b9fcb1c833794f7111308
davidhodge931 <davidhodge931@gmail.com>
2022-06-23 08:10:44 +1200

    Fix #134

--- R/gg_raster.R
+++ R/gg_raster.R
@@ -166,7 +166,8 @@ gg_raster <- function(data = NULL,
       FALSE
     )
 
-    theme <- gg_theme(x_grid = x_grid, y_grid = y_grid)
+  if (x_grid & y_grid) x_grid <- FALSE
+  theme <- gg_theme(x_grid = x_grid, y_grid = y_grid)
   }
 
   if (rlang::is_null(x_zero)) x_zero <- FALSE
@@ -581,8 +582,8 @@ gg_raster <- function(data = NULL,
           }
         }
 
-        if (rlang::is_null(x_limits)) x_limits <- c(min(x_breaks), max(x_breaks))
-        if (rlang::is_null(x_expand)) x_expand <- c(0, 0)
+        if (rlang::is_null(x_limits)) x_limits <- NULL
+        if (rlang::is_null(x_expand)) x_expand <- c(0.025, 0.025)
       }
       else if (facet_scales %in% c("free", "free_x")) {
         if (rlang::is_null(x_breaks)) x_breaks <- ggplot2::waiver()
@@ -733,8 +734,14 @@ gg_raster <- function(data = NULL,
         }
       }
 
-      if (rlang::is_null(x_limits)) x_limits <- c(min(x_breaks), max(x_breaks))
-      if (rlang::is_null(x_expand)) x_expand <- c(0, 0)
+      if (rlang::is_null(x_limits)) {
+        if (!(is.character(rlang::eval_tidy(y, data)) | is.factor(rlang::eval_tidy(y, data)))) x_limits <- NULL
+        else x_limits <- c(min(x_breaks), max(x_breaks))
+      }
+      if (rlang::is_null(x_expand)) {
+        if (!(is.character(rlang::eval_tidy(y, data)) | is.factor(rlang::eval_tidy(y, data)))) x_expand <- c(0.025, 0.025)
+        else x_expand <- c(0, 0)
+      }
     }
     else if (facet_scales %in% c("free", "free_x")) {
       if (rlang::is_null(x_breaks)) x_breaks <- ggplot2::waiver()
