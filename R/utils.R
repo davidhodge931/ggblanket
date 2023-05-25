6e4da620ecbdc922f9f0a9671419e12e41467f77
davidhodge931 <davidhodge931@gmail.com>
2023-05-25 12:23:43 +1200

    Add dev code

--- R/utils.R
+++ R/utils.R
@@ -19,3 +19,25 @@ na_if_double <- function(x) {
 
   x
 }
+
+#' Convert Infinite values to NA
+#'
+#' @param x A vector
+#'
+#' @return A vector
+#' @keywords internal
+na_if_inf <- function(x) {
+  if (is.object(x)) {
+    return(x)
+  }
+
+  if (is.integer(x)) {
+    x <- as.double(x)
+  }
+
+  if (is.numeric(x)) {
+    x <- dplyr::na_if(x, Inf)
+  }
+
+  x
+}
