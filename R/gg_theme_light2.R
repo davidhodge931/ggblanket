e28e8dc33016914525e7ddb237316811b1e9c35e
davidhodge931 <davidhodge931@gmail.com>
2023-06-25 17:38:45 +1200

    Update docs

--- R/gg_theme_light2.R
+++ R/gg_theme_light2.R
@@ -5,16 +5,15 @@ library(ggplot2)
 gg_theme_light2 <- function (
     base_size = 10,
     base_family = "",
-    base_line_size = base_size/33,
-    base_rect_size = base_size/33) {
+    base_pal = "#121b24") {
 
-  half_line <- base_line_size / 2
-
-  theme(line = element_line(colour = "#121b24", linewidth = base_line_size, linetype = 1, lineend = "square"),
-        rect = element_rect(fill = "#e6ecf2", colour = "#e6ecf2", linewidth = base_rect_size, linetype = 1),
+  theme(line = element_line(colour = base_pal, linewidth = base_size/33, linetype = 1, lineend = "square"),
+        rect = element_rect(fill = "#e6ecf2", colour = "#e6ecf2", linewidth = base_size/33, linetype = 1),
         text = element_text(family = base_family, face = "plain",
-                            colour = "#121b24", size = base_size, lineheight = 0.9,
-                            hjust = 0.5, vjust = 0.5, angle = 0,
+                            colour = base_pal, size = base_size, lineheight = 0.9,
+                            hjust = 0.5,
+                            vjust = 0.5,
+                            angle = 0,
                             margin = margin(),
                             debug = FALSE),
         axis.line = NULL,
@@ -27,10 +26,10 @@ gg_theme_light2 <- function (
         axis.text.y = element_text(margin = margin(r = base_size * 0.2), hjust = 1),
         axis.text.y.right = element_text(margin = margin(l = base_size * 0.2), hjust = 0),
 
-        axis.ticks.length.x = grid::unit(base_line_size * 11, "pt"),
+        axis.ticks.length.x = grid::unit(base_size / 3, "pt"),
         axis.ticks.length.x.top = NULL,
         axis.ticks.length.x.bottom = NULL,
-        axis.ticks.length.y = grid::unit(base_line_size * 8.25, "pt"),
+        axis.ticks.length.y = grid::unit(base_size / 4, "pt"),
         axis.ticks.length.y.left = NULL,
         axis.ticks.length.y.right = NULL,
 
@@ -42,7 +41,6 @@ gg_theme_light2 <- function (
 
         legend.spacing = unit(base_size / 8, "pt"),
         legend.spacing.x = NULL,
-        legend.spacing.y = NULL,
         legend.margin = margin(),
         legend.key = NULL,
         legend.key.size = unit(1.2, "lines"),
@@ -61,10 +59,10 @@ gg_theme_light2 <- function (
         legend.box.spacing = NULL,
         panel.background = element_rect(fill = "#fcfdfe", colour = "#fcfdfe"),
         panel.border = element_blank(),
-        panel.grid = element_line(colour = "#DBE1E7", linewidth = rel(0.33)),
-        panel.grid.major = NULL,
+        panel.grid = NULL,
+        panel.grid.major = element_line(colour = "#DBE1E7", linewidth = rel(0.33)),
         panel.grid.minor = element_blank(),
-        panel.spacing = unit(half_line, "pt"),
+        panel.spacing = grid::unit(1.25, "lines"),
         panel.spacing.x = NULL,
         panel.spacing.y = NULL,
         panel.ontop = FALSE,
@@ -80,18 +78,18 @@ gg_theme_light2 <- function (
         strip.placement = "outside",
         strip.placement.x = NULL,
         strip.placement.y = NULL,
-        strip.switch.pad.grid = unit(half_line/2, "pt"),
-        strip.switch.pad.wrap = unit(half_line/2, "pt"),
+        strip.switch.pad.grid = unit(0.15, "pt"),
+        strip.switch.pad.wrap = unit(0.15, "pt"),
 
         plot.background = element_rect(),
         plot.title = element_text(size = rel(1.1), face = "bold", hjust = 0, vjust = 1, margin = margin(t = ((base_size ^ 0.5) * -0.5) - 3.85, b = base_size * 1.75)),
         plot.title.position = "plot",
         plot.subtitle = element_text(hjust = 0, vjust = 1, margin = margin(t = base_size * -1.5, b = base_size * 1.75)),
-        plot.caption = element_text(size = rel(0.9), hjust = 0, vjust = 1, margin = margin(t = base_size * 1.25)),
+        plot.caption = element_text(size = rel(0.9), colour = scales::alpha(base_pal, 0.5), hjust = 0, vjust = 1, margin = margin(t = base_size * 1)),
         plot.caption.position = "plot",
         plot.tag = element_text(size = rel(1.2), hjust = 0, vjust = 0.5),
         plot.tag.position = "topleft",
-        plot.margin = ggplot2::margin(t = 33 * base_line_size, r = 44 * base_line_size, b = 24.2 * base_line_size, l = 22 * base_line_size),
+        plot.margin = ggplot2::margin(t = base_size * 1.5, r = base_size * 2, b = base_size * 1.1, l = base_size * 1),
         complete = TRUE
   )
 }
