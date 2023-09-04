270e0531ecca7341aaf33c4f5bc3276915de1c78
davidhodge931 <davidhodge931@gmail.com>
2023-09-04 17:45:42 +1200

    Fix freqpoly typo

--- R/gg_freqpoly.R
+++ R/gg_freqpoly.R
@@ -368,10 +368,7 @@ gg_freqpoly <- function(
     else pal <- as.vector(pal[1])
 
     plot <- plot +
-      ggplot2::geom_blank(
-        ggplot2::aes(text = !!text), stat = stat,
-        position = position,
-      ) +
+      ggplot2::geom_blank(stat = stat, position = position) +
       ggplot2::geom_freqpoly(
         ggplot2::aes(text = !!text), stat = stat,
         position = position,
@@ -379,16 +376,13 @@ gg_freqpoly <- function(
         col = pal,
         fill = pal,
         ...
-      )
-    coord +
+      ) +
+      coord +
       theme
   }
   else {
     plot <- plot +
-      ggplot2::geom_blank(
-        ggplot2::aes(text = !!text), stat = stat,
-        position = position
-      ) +
+      ggplot2::geom_blank(stat = stat, position = position) +
       ggplot2::geom_freqpoly(
         ggplot2::aes(text = !!text), stat = stat,
         position = position,
