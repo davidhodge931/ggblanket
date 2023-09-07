062a61ff7afe094ade06378e682e5f894adde185
davidhodge931 <davidhodge931@gmail.com>
2023-09-07 17:13:58 +1200

    Fix bug where trying to rev a numeric var

--- R/gg_label.R
+++ R/gg_label.R
@@ -270,7 +270,7 @@ gg_label <- function(
       dplyr::mutate(dplyr::across(!!col, function(x) factor(x)))
   }
 
-  if ((flipped & !col_logical)) {
+  if (flipped & col_character | col_factor) {
     data <- data %>%
       dplyr::mutate(dplyr::across(!!col, function(x) forcats::fct_rev(x)))
   }
