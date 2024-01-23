8a353c71b462817391bf9ef7292306527a755259
davidhodge931 <davidhodge931@gmail.com>
2024-01-24 07:40:52 +1300

    Changes to replace_seq

--- R/replace_seq.R
+++ R/replace_seq.R
@@ -1,12 +1,12 @@
-#' Replace elements of a vector
+#' Replace a sequence of elements in a vector
 #'
-#' @description Replace elements in a vector sequence.
+#' @description Keep every nth element in a vector, and replace the rest with a value such as `""`.
 #'
 #' @param x A vector.
 #' @param ... If numeric, other arguments passed to the `scales::comma` function.
-#' @param keep_nth The increment of elements to keep. Defaults to `2`.
+#' @param keep_nth The increment of elements to keep as is. Defaults to `2`.
 #' @param offset An offset to start at the intended offset. Defaults to `0`. Possible replaces are `-1` to (`keep_nth - 2`)
-#' @param value The value to replace non-kept elements with. Defaults to `""`.
+#' @param replacement The replacement value to replace non-kept elements with. Defaults to `""`.
 #'
 #' @return A vector.
 #' @export
@@ -21,17 +21,17 @@ replace_seq <- function(x,
                         ...,
                         keep_nth = 2,
                         offset = 0,
-                        value = "") {
+                        replacement = "") {
 
   if (is.numeric(x)) {
     replace(scales::comma(x, ...),
             seq_along(scales::comma(x, ...))
-            %% keep_nth != (offset + 1), value)
+            %% keep_nth != (offset + 1), replacement)
   }
   else if (lubridate::is.Date(x) | lubridate::is.POSIXct(x) | hms::is_hms(x)) {
-    replace(as.character(x), seq_along(as.character(x)) %% keep_nth != (offset + 1), value)
+    replace(as.character(x), seq_along(as.character(x)) %% keep_nth != (offset + 1), replacement)
   }
   else {
-    replace(x, seq_along(x) %% keep_nth != (offset + 1), value)
+    replace(x, seq_along(x) %% keep_nth != (offset + 1), replacement)
   }
 }
