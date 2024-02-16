d78140d6e4d757c04122d0f08b977f900a2736ee
davidhodge931 <davidhodge931@gmail.com>
2024-02-16 18:12:52 +1300

    Tidy theme side-effect code

--- R/gg_blanket.R
+++ R/gg_blanket.R
@@ -2097,50 +2097,57 @@ gg_blanket <- function(
 
   if (stringr::str_detect(stat_name, "sf")) {
     plot <- plot +
-      ggplot2::theme(panel.grid.major.x = ggplot2::element_blank()) +
-      ggplot2::theme(panel.grid.minor.x = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.x.bottom = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.x.bottom = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.x.bottom = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.x.bottom = ggplot2::element_blank()) +
+      ggplot2::theme(
+        panel.grid.major.x = ggplot2::element_blank(),
+        panel.grid.minor.x = ggplot2::element_blank(),
+        axis.line.x.top = ggplot2::element_blank(),
+        axis.line.x.bottom = ggplot2::element_blank(),
+        axis.ticks.x.top = ggplot2::element_blank(),
+        axis.ticks.x.bottom = ggplot2::element_blank(),
+        axis.minor.ticks.x.top = ggplot2::element_blank(),
+        axis.minor.ticks.x.bottom = ggplot2::element_blank(),
+        axis.text.x.top = ggplot2::element_blank(),
+        axis.text.x.bottom = ggplot2::element_blank(),
 
-      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank()) +
-      ggplot2::theme(panel.grid.minor.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.y.right = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y.right = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.y.right = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.y.right = ggplot2::element_blank())
+        panel.grid.major.y = ggplot2::element_blank(),
+        panel.grid.minor.y = ggplot2::element_blank(),
+        axis.line.y.left = ggplot2::element_blank(),
+        axis.line.y.right = ggplot2::element_blank(),
+        axis.ticks.y.left = ggplot2::element_blank(),
+        axis.ticks.y.right = ggplot2::element_blank(),
+        axis.minor.ticks.y.left = ggplot2::element_blank(),
+        axis.minor.ticks.y.right = ggplot2::element_blank(),
+        axis.text.y.left = ggplot2::element_blank(),
+        axis.text.y.right = ggplot2::element_blank()
+      )
   }
   else if (flipped) {
     plot <- plot +
-      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank()) +
-      ggplot2::theme(panel.grid.minor.y = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.x.bottom = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.x.bottom = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.x.top = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.x.bottom = ggplot2::element_blank())
+      ggplot2::theme(
+        panel.grid.major.y = ggplot2::element_blank(),
+        panel.grid.minor.y = ggplot2::element_blank(),
+        axis.line.x.top = ggplot2::element_blank(),
+        axis.line.x.bottom = ggplot2::element_blank(),
+        axis.ticks.x.top = ggplot2::element_blank(),
+        axis.ticks.x.bottom = ggplot2::element_blank(),
+        axis.minor.ticks.x.top = ggplot2::element_blank(),
+        axis.minor.ticks.x.bottom = ggplot2::element_blank()
+      )
   }
   else {
     plot <- plot +
-      ggplot2::theme(panel.grid.major.x = ggplot2::element_blank()) +
-      ggplot2::theme(panel.grid.minor.x = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.line.y.right = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.ticks.y.right = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.y.left = ggplot2::element_blank()) +
-      ggplot2::theme(axis.minor.ticks.y.right = ggplot2::element_blank())
+      ggplot2::theme(
+        panel.grid.major.x = ggplot2::element_blank(),
+        panel.grid.minor.x = ggplot2::element_blank(),
+        axis.line.y.left = ggplot2::element_blank(),
+        axis.line.y.right = ggplot2::element_blank(),
+        axis.ticks.y.left = ggplot2::element_blank(),
+        axis.ticks.y.right = ggplot2::element_blank(),
+        axis.minor.ticks.y.left = ggplot2::element_blank(),
+        axis.minor.ticks.y.right = ggplot2::element_blank()
+      )
   }
+
 
   ##############################################################################
   # plot
