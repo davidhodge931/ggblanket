b76868002dc5dc017c53ab8e3d1197fe6d8ad7ec
davidhodge931 <davidhodge931@gmail.com>
2024-02-17 22:09:09 +1300

    Hash out all visual tests

--- tests/testthat/test-modes.R
+++ tests/testthat/test-modes.R
@@ -1,326 +1,326 @@
-testthat::skip_if(getRversion() <= package_version("4.1.0"))
-skip_if_not_installed("vdiffr")
-
-library(palmerpenguins)
-
-test_name <- "light_mode_rt"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = light_mode_rt()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_r"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = light_mode_r()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_t"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = light_mode_t()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_b"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = light_mode_b()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_n"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = light_mode_n()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-###
-
-test_name <- "grey_mode_rt"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = grey_mode_rt()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "grey_mode_r"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = grey_mode_r()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "grey_mode_t"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = grey_mode_t()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "grey_mode_b"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = grey_mode_b()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "grey_mode_n"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = grey_mode_n()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-###
-
-test_name <- "dark_mode_rt"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = dark_mode_rt()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "dark_mode_r"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = dark_mode_r()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "dark_mode_t"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = dark_mode_t()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "dark_mode_b"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = dark_mode_b()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "dark_mode_n"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      col = species,
-      alpha = species,
-      facet = sex,
-      facet2 = island,
-      mapping = aes(shape = species),
-      col_pal = c(teal, orange, plum),
-      alpha_pal = c(1, 1, 0.33),
-      mode = dark_mode_n()
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-
-
+# testthat::skip_if(getRversion() <= package_version("4.1.0"))
+# skip_if_not_installed("vdiffr")
+#
+# library(palmerpenguins)
+#
+# test_name <- "light_mode_rt"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = light_mode_rt()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_r"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = light_mode_r()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_t"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = light_mode_t()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_b"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = light_mode_b()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_n"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = light_mode_n()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# ###
+#
+# test_name <- "grey_mode_rt"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = grey_mode_rt()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "grey_mode_r"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = grey_mode_r()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "grey_mode_t"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = grey_mode_t()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "grey_mode_b"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = grey_mode_b()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "grey_mode_n"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = grey_mode_n()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# ###
+#
+# test_name <- "dark_mode_rt"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = dark_mode_rt()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "dark_mode_r"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = dark_mode_r()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "dark_mode_t"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = dark_mode_t()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "dark_mode_b"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = dark_mode_b()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "dark_mode_n"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       col = species,
+#       alpha = species,
+#       facet = sex,
+#       facet2 = island,
+#       mapping = aes(shape = species),
+#       col_pal = c(teal, orange, plum),
+#       alpha_pal = c(1, 1, 0.33),
+#       mode = dark_mode_n()
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+#
+#
