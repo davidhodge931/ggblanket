b124139d74e013270155a86806320d24ab8fe7f3
davidhodge931 <davidhodge931@gmail.com>
2024-02-18 10:15:00 +1300

    Only skip on >4.1

--- tests/testthat/test-vignette-ggblanket.R
+++ tests/testthat/test-vignette-ggblanket.R
@@ -1,4 +1,5 @@
-testthat::skip_on_os(c("mac", "linux", "solaris"))
+testthat::skip_if(getRversion() <= package_version("4.1.0"))
+# testthat::skip_on_os(c("mac", "linux"))
 
 library(ggplot2)
 library(dplyr)
