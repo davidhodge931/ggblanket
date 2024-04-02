247971827f2bdf967c15a0a990231ba56c6b285e
davidhodge931 <davidhodge931@gmail.com>
2024-04-03 12:10:02 +1300

    Remove NULL logic

--- R/set_blanket.R
+++ R/set_blanket.R
@@ -2,9 +2,9 @@
 #'
 #' @description Set the default style by setting the default mode and updating a series of geom and annotate defaults.
 #'
-#' @param mode A `*_mode_*` set globally for when mode = NULL. E.g. [light_mode_t()], [grey_mode_r()], or [dark_mode_r()]. Use NULL to leave the mode as is.
-#' @param geom_colour A default geom colour. The default geom fill inherits from this. Use NULL to leave geom defaults as is.
-#' @param annotate_colour A default annotate colour used for `*_vline`, `*_hline`, `*_abline`, `*_curve`, `*_text` and `*_label`. The default annotate fill inherits from this. Use NULL to leave annotate defaults as is.
+#' @param mode A `*_mode_*` set globally for when mode = NULL. E.g. [light_mode_t()], [grey_mode_r()], or [dark_mode_r()].
+#' @param geom_colour A default geom colour. The default geom fill inherits from this.
+#' @param annotate_colour A default annotate colour used for `*_vline`, `*_hline`, `*_abline`, `*_curve`, `*_text` and `*_label`. The default annotate fill inherits from this.
 #' @param ... Provided only to support trailing commas.
 #'
 #' @return A globally set mode and updated geom defaults.
@@ -40,8 +40,8 @@ set_blanket <- function(
     annotate_colour = lightness[2],
     ...
 ) {
-  if (!is.null(mode)) weave_mode(mode)
-  if (!is.null(geom_colour)) weave_geom_defaults(geom_colour)
-  if (!is.null(annotate_colour)) weave_annotate_defaults(annotate_colour)
+  weave_mode(mode)
+  weave_geom_defaults(geom_colour)
+  weave_annotate_defaults(annotate_colour)
 }
 
