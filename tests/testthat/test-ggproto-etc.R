276bb558363ac24570ae0ff9ecb775868bcbda58
davidhodge931 <davidhodge931@gmail.com>
2024-04-26 11:04:04 +1200

    Remove ordered code

---
+++ tests/testthat/test-ggproto-etc.R
@@ -0,0 +1,140 @@
+testthat::skip_if(getRversion() <= package_version("4.1.0"))
+testthat::skip_on_os(c("mac", "linux"))
+
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(tidyr)
+library(palmerpenguins)
+set_blanket()
+
+## ---------------------------------------------------------------------------------------------------
+test_name <- "default"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = "violin",
+      stat = "ydensity",
+      position = "dodge",
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+  vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "GeomViolin"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = GeomViolin,
+      stat = "ydensity",
+      position = "dodge",
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "StatYdensity"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = "violin",
+      stat = StatYdensity,
+      position = "dodge",
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "PositionDodge"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = "violin",
+      stat = "ydensity",
+      position = PositionDodge,
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "all 3"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = GeomViolin,
+      stat = StatYdensity,
+      position = PositionDodge,
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "transform_log10"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = GeomViolin,
+      stat = StatYdensity,
+      position = PositionDodge,
+      y_transform = scales::transform_log10(),
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "transform_reverse"
+
+test_that(test_name, {
+  p <- penguins |>
+    drop_na(sex) |>
+    mutate(across(sex, \(x) str_to_sentence(x))) |>
+    gg_blanket(
+      geom = GeomViolin,
+      stat = StatYdensity,
+      position = PositionDodge,
+      y_transform = scales::transform_reverse(),
+      x = sex,
+      y = body_mass_g,
+      col = species,
+    )
+
+    vdiffr::expect_doppelganger(test_name, p)
+})
+
+set_blanket()
---
+++ tests/testthat/test-replace-seq.R
@@ -0,0 +1,58 @@
+test_that("replace_seq works", {
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000)),
+    expected = c("1,000", "", "3,000", "", "5,000", "", "7,000")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), big.mark = " "),
+    expected = c("1 000", "", "3 000", "", "5 000", "", "7 000")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), replacement = "_"),
+    expected = c("1,000", "_", "3,000", "_", "5,000", "_", "7,000")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), keep_nth = 3),
+    expected = c("1,000", "", "", "4,000", "", "", "7,000")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), offset = -1),
+    expected = c("", "2,000", "", "4,000", "", "6,000", "")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), big.mark = " ", keep_nth = 3),
+    expected = c("1 000", "", "", "4 000", "", "", "7 000")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), big.mark = " ", offset = -1),
+    expected = c("", "2 000", "", "4 000", "", "6 000", "")
+  )
+
+  expect_equal(
+    object = replace_seq(seq(1000, 7000, 1000), big.mark = " ", keep_nth = 3, offset = -1),
+    expected = c("", "", "3 000", "", "", "6 000", "")
+  )
+
+  expect_equal(
+    object = replace_seq(LETTERS[1:12]),
+    expected = c("A", "", "C", "", "E", "", "G", "", "I", "", "K", "")
+  )
+
+  expect_equal(
+    object = replace_seq(LETTERS[1:12], keep_nth = 3),
+    expected = c("A", "", "", "D", "", "", "G", "", "", "J", "", "")
+  )
+
+  expect_equal(
+    object = replace_seq(LETTERS[1:12], offset = -1),
+    expected = c("", "B", "", "D", "", "F", "", "H", "", "J", "", "L")
+  )
+
+})
