e22696b88f94ec5eba3c1506f9c1f22a11bd022b
davidhodge931 <davidhodge931@gmail.com>
2024-04-26 11:25:06 +1200

    Update

--- tests/testthat/test-numeric-col.R
+++ tests/testthat/test-numeric-col.R
@@ -1,80 +1,80 @@
-testthat::skip_if(getRversion() <= package_version("4.1.0"))
-testthat::skip_on_os(c("mac", "linux"))
-
-library(palmerpenguins)
-library(ggplot2)
-set_blanket(geom_colour = "#121b24")
-
-test_name <- "light_mode_r"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_jitter(
-      x = flipper_length_mm,
-      y = island,
-      col = bill_depth_mm,
-      facet = sex,
-      mapping = aes(alpha = species, shape = species),
-      mode = light_mode_r()
-    ) +
-    scale_alpha_manual(values = c(1, 1, 0.33))
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_t"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_jitter(
-      x = flipper_length_mm,
-      y = island,
-      col = bill_depth_mm,
-      facet = sex,
-      mapping = aes(alpha = species, shape = species),
-      mode = light_mode_t()
-    ) +
-    scale_alpha_manual(values = c(1, 1, 0.33))
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_b"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_jitter(
-      x = flipper_length_mm,
-      y = island,
-      col = bill_depth_mm,
-      facet = sex,
-      mapping = aes(alpha = species, shape = species),
-      mode = light_mode_b()
-    ) +
-    scale_alpha_manual(values = c(1, 1, 0.33))
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-test_name <- "light_mode_n"
-
-test_that(test_name, {
-
-  p <- penguins |>
-    gg_jitter(
-      x = flipper_length_mm,
-      y = island,
-      col = bill_depth_mm,
-      facet = sex,
-      mapping = aes(alpha = species, shape = species),
-      mode = light_mode_n()
-    ) +
-    scale_alpha_manual(values = c(1, 1, 0.33))
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-set_blanket()
+# testthat::skip_if(getRversion() <= package_version("4.1.0"))
+# testthat::skip_on_os(c("mac", "linux"))
+#
+# library(palmerpenguins)
+# library(ggplot2)
+# set_blanket(geom_colour = "#121b24")
+#
+# test_name <- "light_mode_r"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_jitter(
+#       x = flipper_length_mm,
+#       y = island,
+#       col = bill_depth_mm,
+#       facet = sex,
+#       mapping = aes(alpha = species, shape = species),
+#       mode = light_mode_r()
+#     ) +
+#     scale_alpha_manual(values = c(1, 1, 0.33))
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_t"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_jitter(
+#       x = flipper_length_mm,
+#       y = island,
+#       col = bill_depth_mm,
+#       facet = sex,
+#       mapping = aes(alpha = species, shape = species),
+#       mode = light_mode_t()
+#     ) +
+#     scale_alpha_manual(values = c(1, 1, 0.33))
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_b"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_jitter(
+#       x = flipper_length_mm,
+#       y = island,
+#       col = bill_depth_mm,
+#       facet = sex,
+#       mapping = aes(alpha = species, shape = species),
+#       mode = light_mode_b()
+#     ) +
+#     scale_alpha_manual(values = c(1, 1, 0.33))
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# test_name <- "light_mode_n"
+#
+# test_that(test_name, {
+#
+#   p <- penguins |>
+#     gg_jitter(
+#       x = flipper_length_mm,
+#       y = island,
+#       col = bill_depth_mm,
+#       facet = sex,
+#       mapping = aes(alpha = species, shape = species),
+#       mode = light_mode_n()
+#     ) +
+#     scale_alpha_manual(values = c(1, 1, 0.33))
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# set_blanket()
--- tests/testthat/test-positional_scales.R
+++ tests/testthat/test-positional_scales.R
@@ -1,86 +1,86 @@
-testthat::skip_if(getRversion() <= package_version("4.1.0"))
-testthat::skip_on_os(c("mac", "linux"))
-
-library(ggplot2)
-library(dplyr)
-set_blanket()
-
-## ---------------------------------------------------------------------------------------------------
-test_name <- "1"
-
-test_that(test_name, {
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      x_breaks = scales::breaks_width(-15),
-      x_expand_limits = 250,
-      x_labels = scales::label_currency(),
-      x_transform = scales::transform_reverse(),
-      x_limits = c(200, 170),
-      x_oob = scales::oob_censor,
-      x_expand = c(0.5, 0.5),
-      x_position = "top",
-      x_title = "Blah",
-      caption = ""
-  )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-## ---------------------------------------------------------------------------------------------------
-test_name <- "2"
-
-test_that(test_name, {
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      y_breaks = scales::breaks_width(-1000),
-      y_expand_limits = 7000,
-      y_labels = scales::label_currency(),
-      y_transform = scales::transform_reverse(),
-      y_limits = c(6000, 2000),
-      y_oob = scales::oob_censor,
-      y_expand = c(0.1, 0.1),
-      y_position = "right",
-      y_title = "Blah",
-      caption = ""
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-## ---------------------------------------------------------------------------------------------------
-test_name <- "3"
-
-test_that(test_name, {
-  p <- penguins |>
-    gg_point(
-      x = flipper_length_mm,
-      y = body_mass_g,
-      x_breaks = scales::breaks_width(-15),
-      x_expand_limits = 250,
-      x_labels = scales::label_currency(),
-      x_transform = scales::transform_reverse(),
-      x_limits = c(200, 170),
-      x_oob = scales::oob_censor,
-      x_expand = c(0.5, 0.5),
-      x_position = "top",
-      x_title = "Blah",
-      y_breaks = scales::breaks_width(-1000),
-      y_expand_limits = 7000,
-      y_labels = scales::label_currency(),
-      y_transform = scales::transform_reverse(),
-      y_limits = c(6000, 2000),
-      y_oob = scales::oob_censor,
-      y_expand = c(0.1, 0.1),
-      y_position = "right",
-      y_title = "Blah",
-      caption = ""
-    )
-
-  vdiffr::expect_doppelganger(test_name, p)
-})
-
-set_blanket()
+# testthat::skip_if(getRversion() <= package_version("4.1.0"))
+# testthat::skip_on_os(c("mac", "linux"))
+#
+# library(ggplot2)
+# library(dplyr)
+# set_blanket()
+#
+# ## ---------------------------------------------------------------------------------------------------
+# test_name <- "1"
+#
+# test_that(test_name, {
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       x_breaks = scales::breaks_width(-15),
+#       x_expand_limits = 250,
+#       x_labels = scales::label_currency(),
+#       x_transform = scales::transform_reverse(),
+#       x_limits = c(200, 170),
+#       x_oob = scales::oob_censor,
+#       x_expand = c(0.5, 0.5),
+#       x_position = "top",
+#       x_title = "Blah",
+#       caption = ""
+#   )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# ## ---------------------------------------------------------------------------------------------------
+# test_name <- "2"
+#
+# test_that(test_name, {
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       y_breaks = scales::breaks_width(-1000),
+#       y_expand_limits = 7000,
+#       y_labels = scales::label_currency(),
+#       y_transform = scales::transform_reverse(),
+#       y_limits = c(6000, 2000),
+#       y_oob = scales::oob_censor,
+#       y_expand = c(0.1, 0.1),
+#       y_position = "right",
+#       y_title = "Blah",
+#       caption = ""
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# ## ---------------------------------------------------------------------------------------------------
+# test_name <- "3"
+#
+# test_that(test_name, {
+#   p <- penguins |>
+#     gg_point(
+#       x = flipper_length_mm,
+#       y = body_mass_g,
+#       x_breaks = scales::breaks_width(-15),
+#       x_expand_limits = 250,
+#       x_labels = scales::label_currency(),
+#       x_transform = scales::transform_reverse(),
+#       x_limits = c(200, 170),
+#       x_oob = scales::oob_censor,
+#       x_expand = c(0.5, 0.5),
+#       x_position = "top",
+#       x_title = "Blah",
+#       y_breaks = scales::breaks_width(-1000),
+#       y_expand_limits = 7000,
+#       y_labels = scales::label_currency(),
+#       y_transform = scales::transform_reverse(),
+#       y_limits = c(6000, 2000),
+#       y_oob = scales::oob_censor,
+#       y_expand = c(0.1, 0.1),
+#       y_position = "right",
+#       y_title = "Blah",
+#       caption = ""
+#     )
+#
+#   vdiffr::expect_doppelganger(test_name, p)
+# })
+#
+# set_blanket()
