f49315b5656d07246a0d5f3800f7e7ae6c1942b5
davidhodge931 <davidhodge931@gmail.com>
2024-05-09 08:28:53 +1200

    Update

--- R/hold.R
+++ R/hold.R
@@ -1,19 +1,19 @@
-#' Hold by every nth element
+#' Hold by every n element
 #'
 #' @description
-#' Hold by every nth element in a vector, and replace the rest with "".
+#' Hold by every n element in a vector, and replace the rest with "".
 #'
-#' @param nth The increment of elements to hold as is. Defaults to `2`.
-#' @param offset An offset for which element to first hold. Defaults to `0`. Possible values are `-1` to (`nth - 2`)
+#' @param n The increment of elements to hold as is. Defaults to `2`.
+#' @param offset An offset for which element to first hold. Defaults to `0`. Possible values are `-1` to (`n - 2`)
 #' @param ... If numeric, other arguments passed to the `scales::comma` function.
 #'
 #' @return A character vector
 #' @noRd
 #'
 #' @examples
-#' hold_by_nth()scales::comma(seq(1000, 5000, 1000))
-#' hold_by_nth()format(lubridate::ymd(c("2021-01-01", "2022-01-01", "2023-01-01", "2024-01-01")))
-#' hold_by_nth()LETTERS[1:12]
+#' hold_every_n()scales::comma(seq(1000, 5000, 1000))
+#' hold_every_n()format(lubridate::ymd(c("2021-01-01", "2022-01-01", "2023-01-01", "2024-01-01")))
+#' hold_every_n()LETTERS[1:12]
 #'
 #' library(dplyr)
 #' library(palmerpenguins)
@@ -26,15 +26,15 @@
 #'     x = flipper_length_mm,
 #'     y = body_mass_g,
 #'     col = sex,
-#'     y_labels = hold_by_nth(),
+#'     y_labels = hold_every_n(),
 #'   )
 #
-hold_by_nth <- function(nth = 2, offset = 0, ...) {
-  force(nth)
+hold_every_n <- function(n = 2, offset = 0, ...) {
+  force(n)
   force(offset)
   function(x) {
     i <- which(is.finite(x) | is.character(x) | is.factor(x) | is.logical(x))
-    i <- i[seq_along(i) %% nth == (offset + 1)]
+    i <- i[seq_along(i) %% n == (offset + 1)]
     if (is.numeric(x)) x <- scales::comma(x, ...)
     else x <- as.character(x)
     x[-i] <- ""
