bd95ff8ab17cc322658fd869cf722f569dddc618
davidhodge931 <davidhodge931@gmail.com>
2024-05-13 20:09:15 +1200

    Update docs

---
+++ R/colour.R
@@ -0,0 +1,65 @@
+#' A blue colour
+#'
+#' @description A blue colour.
+#'
+#' @format NULL
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' scales::show_col(blue)
+blue <- "#357ba2ff"
+
+#' A grey colour
+#'
+#' @description A grey colour.
+#'
+#' @format NULL
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' scales::show_col(grey)
+grey <- "#cdc5bfff"
+
+#' The jumble palette
+#'
+#' @description A discrete palette with 6 colours that is relatively colour-blind safe. Add grey for an extra colour, and have no NA values.
+#'
+#' @format NULL
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' colorspace::swatchplot(jumble, cvd = TRUE)
+jumble <- c("#0095a8ff", "#ffa600ff", "#003f5cff", "#da3c39ff", "#ec9ecbff", "#67609cff")
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+teal <- "#0095a8ff"
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+orange <- "#ffa600ff"
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+navy <- "#003f5cff"
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+red <- "#da3c39ff"
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+pink <- "#ec9ecbff"
+
+#' @rdname jumble
+#' @format NULL
+#' @export
+purple <- "#67609cff"
