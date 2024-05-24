4b8ea30601927a539a64e1419f92dd071bb53f5c
davidhodge931 <davidhodge931@gmail.com>
2024-05-25 11:50:53 +1200

    Add orientation tests

---
+++ tests/testthat/test-orientation.R
@@ -0,0 +1,59 @@
+testthat::skip_if(getRversion() <= package_version("4.1.0"))
+testthat::skip_on_os(c("mac", "linux"))
+
+library(palmerpenguins)
+library(ggplot2)
+
+set_blanket()
+
+test_name <- "1"
+
+test_that(test_name, {
+
+  p <- penguins |>
+    gg_histogram(
+      y = flipper_length_mm,
+      y_orientation = FALSE,
+    )
+
+  vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "2"
+
+test_that(test_name, {
+
+  p <- penguins |>
+    gg_histogram(
+      y = flipper_length_mm,
+      x_orientation = TRUE,
+    )
+
+  vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "3"
+
+test_that(test_name, {
+
+  p <- penguins |>
+    gg_histogram(
+      y = flipper_length_mm,
+      y_orientation = TRUE,
+    )
+
+  vdiffr::expect_doppelganger(test_name, p)
+})
+
+test_name <- "4"
+
+test_that(test_name, {
+
+  p <- penguins |>
+    gg_histogram(
+      y = flipper_length_mm,
+      x_orientation = FALSE,
+    )
+
+  vdiffr::expect_doppelganger(test_name, p)
+})
