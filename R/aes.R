4909e8879012c78319bbd2b4a056cce10c4ec745
davidhodge931 <davidhodge931@gmail.com>
2024-07-23 21:44:41 +1200

    Add aes lighten and darken functions #957

--- R/aes.R
+++ R/aes.R
@@ -32,7 +32,7 @@ get_colour_contrast <- function(fill,
 #' @param dark A dark colour.
 #' @param light A light colour.
 #'
-#' @return An aesthetic
+#' @return A ggplot2 aesthetic
 #' @export
 #'
 #' @examples
@@ -85,4 +85,69 @@ aes_colour_contrast <- function(..., dark = "#121B24FF", light = "#FFFFFFFF") {
       get_colour_contrast(.data$fill, dark = dark, light = light)
     )
   )
+}
+
+#' Lighten/darken a colour/fill aesthetic
+#'
+#' @description
+#' Lighten/darken a colour/fill aesthetic based on a
+#' * `aes_colour_darken()` Darken a colour aesthetic, relative to a fill aesthetic
+#' * `aes_colour_lighten()` Lighten a colour aesthetic, relative to the fill aesthetic
+#' * `aes_fill_darken()` Darken a fill aesthetic, relative to a colour aesthetic
+#' * `aes_fill_lighten()` Lighten a fill aesthetic, relative to the colour aesthetic
+#' Can be spliced into [ggplot2::aes] with [rlang::!!!].
+#'
+#' @param ... Arguments passed to [colorspace::darken()]/[colorspace::lighten()] (excluding `col`).
+#'
+#' @return A ggplot2 aesthetic
+#' @export
+#'
+#' @examples
+#' library(ggblanket)
+#' library(ggplot2)
+#' library(palmerpenguins)
+#'
+#' set_blanket(
+#'   alpha_recursive = 1,
+#' )
+#'
+#' penguins |>
+#'   gg_bar(
+#'     y = species,
+#'     col = island,
+#'     mapping = aes_colour_darken(amount = 0.2),
+#'     width = 0.75,
+#'   )
+#'
+#' penguins |>
+#' gg_bar(
+#'   y = species,
+#'   col = island,
+#'   mapping = aes(!!!aes_colour_darken(amount = 0.2)),
+#'   width = 0.75,
+#' )
+#'
+aes_colour_darken <- function(...) {
+  ggplot2::aes(colour = ggplot2::after_scale(colorspace::darken(.data$fill, ...)))
+}
+
+#' @rdname aes_colour_darken
+#' @export
+#'
+aes_colour_lighten <- function(...) {
+  ggplot2::aes(colour = ggplot2::after_scale(colorspace::lighten(.data$fill, ...)))
+}
+
+#' @rdname aes_colour_darken
+#' @export
+#'
+aes_fill_darken <- function(...) {
+  ggplot2::aes(fill = ggplot2::after_scale(colorspace::darken(.data$colour, ...)))
+}
+
+#' @rdname aes_colour_darken
+#' @export
+#'
+aes_fill_lighten <- function(...) {
+  ggplot2::aes(fill = ggplot2::after_scale(colorspace::lighten(.data$colour, ...)))
 }
