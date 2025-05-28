0701a2d54562e4b7143970b3bf612d092559453b
davidhodge931 <davidhodge931@gmail.com>
2025-05-29 11:39:42 +1200

    Change name of var in bind_all_by

--- R/bind_all_by.R
+++ R/bind_all_by.R
@@ -9,7 +9,7 @@
 #' @param all A character value for the all value. Defaults to `"All"`.
 #' @param all_after A number for where the all value should be placed after. Either 0 for first or Inf for last.
 #' @param groups A character value for the group value. Defaults to `"Groups"`.
-#' @param name A variable name. Defaults to `all_or_groups`.
+#' @param name A variable name. Defaults to `groups_or_all`.
 #'
 #' @return A data frame or tibble
 #' @export
@@ -23,7 +23,7 @@
 #'
 #' penguins |>
 #'   bind_all_by(species) |>
-#'   distinct(species, all_or_groups)
+#'   distinct(species, groups_or_all)
 #'
 #' penguins |>
 #'   bind_all_by(species) |>
@@ -37,19 +37,19 @@
 #'   gg_jitter(
 #'     x = species,
 #'     y = body_mass_g,
-#'     col = all_or_groups,
+#'     col = groups_or_all,
 #'     col_palette = c(blue, grey),
 #'   ) +
 #'   theme(legend.position = "none")
 #'
 #' penguins |>
 #'   bind_all_by(species) |>
-#'   group_by(species, all_or_groups) |>
+#'   group_by(species, groups_or_all) |>
 #'   summarise(across(body_mass_g, \(x) mean(x, na.rm = TRUE))) |>
 #'   gg_col(
 #'     x = species,
 #'     y = body_mass_g,
-#'     col = all_or_groups,
+#'     col = groups_or_all,
 #'     col_palette = c(blue, grey),
 #'     width = 0.5,
 #'     y_label = "Average body mass g",
@@ -61,9 +61,9 @@
 #'   gg_jitter(
 #'     x = species,
 #'     y = body_mass_g,
-#'     col = all_or_groups,
+#'     col = groups_or_all,
 #'     col_palette = c(blue, grey),
-#'     facet = all_or_groups,
+#'     facet = groups_or_all,
 #'     facet_layout = "grid",
 #'     facet_scales = "free_x",
 #'     facet_space = "free_x",
@@ -78,7 +78,7 @@ bind_all_by <- function(data,
                         all = "All",
                         all_after = Inf,
                         groups = "Groups",
-                        name = "all_or_groups") {
+                        name = "groups_or_all") {
 
   by <- rlang::enquo(by)
 
