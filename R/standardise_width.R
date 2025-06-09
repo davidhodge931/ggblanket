e814d562f958494c55119c47f83c475c39c0c7bb
davidhodge931 <davidhodge931@gmail.com>
2025-06-10 10:04:21 +1200

    Update

--- R/standardise_width.R
+++ R/standardise_width.R
@@ -5,28 +5,35 @@
 #' Save plots with uniform panel dimensions.
 #'
 #' Note: intended for polygons.
-#' Does not work with:
-#' * panel.sizes of different dimensions
-#' * geoms that are dodged etc.
+#' Works with position_dodge and position_dodge2.
 #'
 #' @param n Number of categories.
 #' @param n_dodge Number of groups dodged.
+#' @param padding Padding for position_dodge2. Defaults to 0.
 #' @param reference_n Number of categories in the reference plot. Defaults to 3.
-#' @param reference_n_dodge Number of groups dodged.in the reference plot. Defaults to 1.
-#' @param reference_width Width value in the reference plot. Defaults to 0.2.
+#' @param reference_n_dodge Number of groups dodged in the reference plot. Defaults to 1.
+#' @param reference_width Width value in the reference plot. Defaults to 0.5.
+#' @param reference_padding Padding for position_dodge2 in the reference plot. Defaults to 0.
 #'
 #' @returns A numeric value
 #' @noRd
 #'
 #' @examples
-#' # For a plot with 2 bars, standardised to a 3-bar reference with 0.2 width
+#' # For a plot with 2 bars, standardised to a 3-bar reference with 0.5 width
 #' standardise_width(2)
+#'
+#' # For dodged bars
+#' standardise_width(n = 3, n_dodge = 2)
+#'
+#' # For position_dodge2 with padding
+#' standardise_width(n = 3, n_dodge = 2, padding = 0.1)
 standardise_width <- function(n,
                               n_dodge = 1,
+                              padding = 0,
                               reference_n = 3,
                               reference_n_dodge = 1,
-                              reference_width = 0.5) {
-
+                              reference_width = 0.5,
+                              reference_padding = 0) {
   # Base category scaling
   base_width <- (n / reference_n) * reference_width
 
@@ -35,6 +42,13 @@ standardise_width <- function(n,
     width <- base_width * (n_dodge / reference_n_dodge)
   } else {
     width <- base_width
+  }
+
+  # Padding adjustment for position_dodge2
+  if (padding > 0 || reference_padding > 0) {
+    # Adjust for difference in padding - more padding requires wider total width
+    padding_factor <- (1 + padding) / (1 + reference_padding)
+    width <- width * padding_factor
   }
 
   if (width >= 1)
