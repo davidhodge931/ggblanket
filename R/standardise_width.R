c6a7d7a948fbceed9f81cf0af359cdfc1f812d31
davidhodge931 <davidhodge931@gmail.com>
2025-06-10 10:41:31 +1200

    Update

--- R/standardise_width.R
+++ R/standardise_width.R
@@ -7,13 +7,13 @@
 #' Note: intended for polygons.
 #' Works with position_dodge and position_dodge2.
 #'
-#' @param n Number of categories.
-#' @param n_dodge Number of groups dodged.
-#' @param padding Padding for position_dodge2. Defaults to 0.
-#' @param reference_n Number of categories in the reference plot. Defaults to 3.
-#' @param reference_n_dodge Number of groups dodged in the reference plot. Defaults to 1.
-#' @param reference_width Width value in the reference plot. Defaults to 0.5.
-#' @param reference_padding Padding for position_dodge2 in the reference plot. Defaults to 0.
+#' @param n Number of x aesthetic groups.
+#' @param dodge_n Number of fill aesthetic etc groups dodged.
+#' @param dodge_padding Amount of padding (in position_dodge2). Defaults to 0.
+#' @param reference_width In the reference, width value. Defaults to 0.5.
+#' @param reference_n In the reference, number of x aesthetic groups. Defaults to 3.
+#' @param reference_dodge_n In the reference, number of fill aesthetic etc groups dodged. Defaults to 1.
+#' @param reference_dodge_padding In the reference, amount of padding (in position_dodge2). Defaults to 0.
 #'
 #' @returns A numeric value
 #' @noRd
@@ -23,32 +23,32 @@
 #' standardise_width(2)
 #'
 #' # For dodged bars
-#' standardise_width(n = 3, n_dodge = 2)
+#' standardise_width(n = 3, dodge_n = 2)
 #'
-#' # For position_dodge2 with padding
-#' standardise_width(n = 3, n_dodge = 2, padding = 0.1)
+#' # For position_dodge2 with dodge_padding
+#' standardise_width(n = 3, dodge_n = 2, dodge_padding = 0.1)
 standardise_width <- function(n,
-                              n_dodge = 1,
-                              padding = 0,
-                              reference_n = 3,
-                              reference_n_dodge = 1,
+                              dodge_n = 1,
+                              dodge_padding = 0,
                               reference_width = 0.5,
-                              reference_padding = 0) {
+                              reference_n = 3,
+                              reference_dodge_n = 1,
+                              reference_dodge_padding = 0) {
   # Base category scaling
   base_width <- (n / reference_n) * reference_width
 
   # Dodge scaling: maintain consistent individual bar width
-  if (n_dodge > 1 || reference_n_dodge > 1) {
-    width <- base_width * (n_dodge / reference_n_dodge)
+  if (dodge_n > 1 || reference_dodge_n > 1) {
+    width <- base_width * (dodge_n / reference_dodge_n)
   } else {
     width <- base_width
   }
 
-  # Padding adjustment for position_dodge2
-  if (padding > 0 || reference_padding > 0) {
-    # Adjust for difference in padding - more padding requires wider total width
-    padding_factor <- (1 + padding) / (1 + reference_padding)
-    width <- width * padding_factor
+  # dodge_padding adjustment for position_dodge2
+  if (dodge_padding > 0 || reference_dodge_padding > 0) {
+    # Adjust for difference in dodge_padding - more dodge_padding requires wider total width
+    dodge_padding_factor <- (1 + dodge_padding) / (1 + reference_dodge_padding)
+    width <- width * dodge_padding_factor
   }
 
   if (width >= 1)
