bf90a33739fffc782bbb2bcdebddd874549e89db
davidhodge931 <davidhodge931@gmail.com>
2025-06-10 09:29:18 +1200

    Update standardised with for dodge

--- R/standardise_width.R
+++ R/standardise_width.R
@@ -9,21 +9,35 @@
 #' * panel.sizes of different dimensions
 #' * geoms that are dodged etc.
 #'
-#' @param n Number of categories in the current plot
+#' @param n Number of categories.
+#' @param n_dodge Number of groups dodged.
 #' @param reference_n Number of categories in the reference plot. Defaults to 3.
-#' @param reference_width Width value in the reference plot. Defaults to 0.5.
+#' @param reference_n_dodge Number of groups dodged.in the reference plot. Defaults to 1.
+#' @param reference_width Width value in the reference plot. Defaults to 0.2.
 #'
 #' @returns A numeric value
 #' @noRd
 #'
 #' @examples
-#' # For a plot with 2 bars, standardised to a 3-bar reference
+#' # For a plot with 2 bars, standardised to a 3-bar reference with 0.2 width
 #' standardise_width(2)
-#'
-#' # For a plot with 5 bars
-#' standardise_width(5)
-standardise_width <- function(n, reference_n = 3, reference_width = 0.5) {
-  width <- (n / reference_n) * reference_width
-  if (width >= 1) rlang::abort("width cannot be greater than or equal to 1")
+standardise_width <- function(n,
+                              n_dodge = 1,
+                              reference_n = 3,
+                              reference_n_dodge = 1,
+                              reference_width = 0.5) {
+
+  # Base category scaling
+  base_width <- (n / reference_n) * reference_width
+
+  # Dodge scaling: maintain consistent individual bar width
+  if (n_dodge > 1 || reference_n_dodge > 1) {
+    width <- base_width * (n_dodge / reference_n_dodge)
+  } else {
+    width <- base_width
+  }
+
+  if (width >= 1)
+    rlang::abort("width cannot be greater than or equal to 1")
   return(width)
 }
