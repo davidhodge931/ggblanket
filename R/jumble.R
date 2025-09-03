2e595f39c9cd4ac85c1536109a191586867b9d81
davidhodge931 <davidhodge931@gmail.com>
2025-09-03 13:07:52 +1200

    Add colours back in

--- R/jumble.R
+++ R/jumble.R
@@ -4,8 +4,6 @@
 #' A jumble of useful colours.
 #' The first 6 are relatively colour-blind safe on a light background.
 #' The 7th is a oat colour that can be used for NA.
-#' The 8th is a slate colour that can be used in guide keys etc.
-#' The 9th and 10th are blue colours that be used as a default colour/fill set aesthetic.
 #'
 #' @format NULL
 #' @return A character vector.
@@ -20,8 +18,85 @@ jumble <- c(
   "#DA3C39FF",
   "#EC9ECBFF",
   "#67609CFF",
-  "#CDC5BFFF",
-  "#8991A1FF",
-  "#4797C3FF",
-  "#357BA2FF"
+  "#CDC5BFFF"
 )
+
+#' Teal color
+#' @export
+#' @examples
+#' scales::show_col(teal)
+teal <- "#0095A8FF"
+
+#' Orange color
+#' @export
+#' @examples
+#' scales::show_col(orange)
+orange <- "#FFA600FF"
+
+#' Navy color
+#' @export
+#' @examples
+#' scales::show_col(navy)
+navy <- "#003F5CFF"
+
+#' Red color
+#' @export
+#' @examples
+#' scales::show_col(red)
+red <- "#DA3C39FF"
+
+#' Pink color
+#' @export
+#' @examples
+#' scales::show_col(pink)
+pink <- "#EC9ECBFF"
+
+#' Purple color
+#' @export
+#' @examples
+#' scales::show_col(purple)
+purple <- "#67609CFF"
+
+#' Oat color
+#' @export
+#' @examples
+#' scales::show_col(oat)
+oat <- "#CDC5BFFF"
+
+#' Black color (from Flexoki base palette)
+#' @export
+#' @examples
+#' scales::show_col(black)
+black <- "#1C1B1Aff"
+
+#' Grey color
+#' @export
+#' @examples
+#' scales::show_col(grey)
+grey <- "#f2f2f2ff"
+
+#' Beige color (from Flexoki base palette)
+#' @export
+#' @examples
+#' scales::show_col(beige)
+beige <- "#F2F0E5ff"
+
+#' Slate color
+#' @export
+#' @examples
+#' scales::show_col(slate)
+slate <- "#8991A1FF"
+
+#' Blue color
+#' @export
+#' @examples
+#' scales::show_col(blue)
+blue <- "#4797C3FF"
+
+#' Ocean color
+#' @export
+#' @examples
+#' scales::show_col(ocean)
+ocean <- "#357BA2FF"
+
+
